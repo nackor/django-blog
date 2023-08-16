4547bb90bd0b61cf8a87f0358cdb2f83da8b36e8
Victor Long <nackor91@gmail.com>
2023-08-16 16:57:16 -0700

    stub view outbuts now

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),  # <-- Add this
+    path('polling/', include('polling.urls')), 
     path('admin/', admin.site.urls),
+    path('', include('blogging.urls'))  
 ]
