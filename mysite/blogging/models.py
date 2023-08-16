3dee104e688984135c365f6d11ca67e2a09eb286
Victor Long <nackor91@gmail.com>
2023-08-16 13:36:46 -0700

    added date times

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -4,9 +4,9 @@ class Post(models.Model):
     title = models.CharField(max_length=128)
     body = models.TextField(blank=True)
     author = models.CharField(default=0)
-    created_date 
-    modified_date
-    published_date
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
