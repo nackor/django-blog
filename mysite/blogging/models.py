bc58a5ed6aca51963cf50bcdcd045f3fae0e99c2
Victor Long <nackor91@gmail.com>
2023-08-16 13:35:35 -0700

    starting on models for blogging

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.TextField(blank=True)
+    author = models.CharField(default=0)
+    created_date 
+    modified_date
+    published_date
+
+    def __str__(self):
+        return self.title
