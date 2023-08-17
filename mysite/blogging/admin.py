8853d3dd873a8d97ab457d7e7e4ee0fe532c408a
Victor Long <nackor91@gmail.com>
2023-08-17 10:10:13 -0700

    doing exclude first

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -3,4 +3,9 @@ from blogging.models import Post, Category
 
 # and a new admin registration
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts')
+
+
 admin.site.register(Category)   
