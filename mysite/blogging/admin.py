234931d1ee63b471a310cac0591fe9651b91e147
Victor Long <nackor91@gmail.com>
2023-08-17 10:18:46 -0700

    working on in  line add

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     exclude = ('posts',)
 
+class CategoryInLine(admin.TabularInline):
+    pass
+class PostAdmin(admin.ModelAdmin):
+    pass
 
+
+
+admin.site.register(Post)
 admin.site.register(Category, CategoryAdmin) 
