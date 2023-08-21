3f066204a13f32f6596432b7549f8c6b2ba735db
Victor Long <nackor91@gmail.com>
2023-08-20 21:43:34 -0700

    Revert "fix pk bug="
    This reverts commit bcb54b736fcd938bfd279ff9bfea06dd4f9cbced.

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blogging.views import stub_view,BlogListView,BlogDetailView
+from blogging.views import stub_view,list_view,detail_view
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name="blog_index"),
-    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail")
+    path('', PollListView.as_view(), name="blog_index"),
+    path("posts/<int:post_id>/", PollDetailView.as_view(), name="blog_detail")
 ]
