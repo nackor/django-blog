01421c8ac0ee7ee69510c3c818f6579efeb5d455
Victor Long <nackor91@gmail.com>
2023-08-20 21:43:40 -0700

    Revert "working on class views"
    This reverts commit 4f8b81640876f1eee3e07a08d44a99d1abfb4063.

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blogging.views import stub_view,list_view,detail_view
 
 urlpatterns = [
-    path('', PollListView.as_view(), name="blog_index"),
-    path("posts/<int:post_id>/", PollDetailView.as_view(), name="blog_detail")
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
 ]
--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -2,17 +2,6 @@ from django import http
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from blogging.models import Post
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-
-class BlogListView(ListView):
-    model = Post
-    template_name = 'blogging/list.html'
-
-class BlogDetailView(DetailView):
-    model = Post
-    template_name = 'blogging/detail.html'
-
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
