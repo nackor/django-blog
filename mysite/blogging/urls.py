4f8b81640876f1eee3e07a08d44a99d1abfb4063
Victor Long <nackor91@gmail.com>
2023-08-20 18:18:29 -0700

    working on class views

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blogging.views import stub_view,list_view,detail_view
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', PollListView.as_view(), name="blog_index"),
+    path("posts/<int:post_id>/", PollDetailView.as_view(), name="blog_detail")
 ]
