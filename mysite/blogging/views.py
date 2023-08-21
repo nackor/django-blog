51413b9961b27d3cf37f9db597f6ac51d075cc3a
Victor Long <nackor91@gmail.com>
2023-08-20 21:40:02 -0700

    delete old view functions

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -24,22 +24,3 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-
-def list_view(request):
-    filtered_posts =  Post.objects.exclude(published_date__isnull=True)
-    context = {
-        'posts': filtered_posts,
-        'empty': len(filtered_posts)
-        }
-    return render(request, 'blogging/list.html', context)
-
-def detail_view(request, post_id):
-    try:
-        post = Post.objects.get(pk=post_id)
-        if(post.published_date == None):
-            raise Http404
-    except Post.DoesNotExist:
-        raise Http404
-
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
